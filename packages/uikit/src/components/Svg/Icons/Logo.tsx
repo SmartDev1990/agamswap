dcd7d44855b2188076c03c4bf4f2b174e2dc986b
Danijel-Enoch <enochdanijel@gmail.com>
2022-09-26 22:11:57 +0100

    logo debugging

--- packages/uikit/src/components/Svg/Icons/Logo.tsx
+++ packages/uikit/src/components/Svg/Icons/Logo.tsx
@@ -3,11 +3,11 @@ import Svg from "../Svg";
 import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
-  return (
-    <Svg viewBox="0 0 198 199" {...props}>
-      <image width="198" height="198" href="/images/Home/lunar-Bunny/Bunny@2x.png" />
-    </Svg>
-  );
+	return (
+		<Svg viewBox="0 0 198 199" {...props}>
+			<image width="198" height="198" href="https://i.ibb.co/dJQVmgf/1.png" />
+		</Svg>
+	);
 };
 
 export default Icon;
